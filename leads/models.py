c81496ec9815979feaf4aba92582ec3840b5a87a
Raihan Ahmed Dip <raihan.dip@gmail.com>
2022-02-23 01:32:55 +0600

    Creating a User class from the AbstractUser class and also have a Agent class with OneToOne relationship with User class. Lead class is also modified to have a foreign relationship with Agent Class so that one single Agent can have multiple Leads.

--- leads/models.py
+++ leads/models.py
@@ -1,22 +1,23 @@
-from pyexpat import model
 from django.db import models
+#from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+
+
+#User = get_user_model()
+
+class User(AbstractUser):
+    pass
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #first_name = models.CharField(max_length=100)
+    #last_name = models.CharField(max_length=100)
 
 
 class Lead(models.Model):
-
-    SOURCE_CHOICES = (
-        ('YouTube', 'YouTube'),
-        ('Facebook', 'Facebook'),
-        ('Google', 'Google'),
-        ('Newsletter', 'Newsletter'),
-    )
-
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     age = models.IntegerField(default=0)
 
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
-
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
+    agent = models.ForeignKey("Agent", on_delete=models.SET_NULL, null=True)
