10d1339eb589d20d78f5aa054568b9998d95a232
Raihan Ahmed Dip <raihan.dip@gmail.com>
2022-02-23 01:01:49 +0600

    Inital model created with basic fields for the lead's model. We use touple as source of choice there. Good reference point to see.

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,22 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+
+    SOURCE_CHOICES = (
+        ('YouTube', 'YouTube'),
+        ('Facebook', 'Facebook'),
+        ('Google', 'Google'),
+        ('Newsletter', 'Newsletter'),
+    )
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+
+    profile_picture = models.ImageField(blank=True, null=True)
+    special_files = models.FileField(blank=True, null=True)
