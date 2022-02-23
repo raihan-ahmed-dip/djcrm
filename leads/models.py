cd9ab23e34508fd08da8048d53cf4bc553918133
Raihan Ahmed Dip <raihan.dip@gmail.com>
2022-02-23 22:56:26 +0600

    String return for model class

--- leads/models.py
+++ leads/models.py
@@ -14,10 +14,16 @@ class Agent(models.Model):
     #first_name = models.CharField(max_length=100)
     #last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.user.email
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     age = models.IntegerField(default=0)
 
-    agent = models.ForeignKey("Agent", on_delete=models.SET_NULL, null=True)
+    agent = models.ForeignKey(Agent, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
