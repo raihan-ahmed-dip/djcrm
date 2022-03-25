e713481d88e50144843fe689a7832bbdd84fadb2
Raihan Ahmed Dip <raihan.dip@gmail.com>
2022-03-26 00:28:48 +0600

    Added test into the system.

---
+++ leads/tests/test_views.py
@@ -0,0 +1,15 @@
+from audioop import reverse
+from urllib import response
+from django.test import TestCase
+from django.shortcuts import reverse
+
+
+# Create your tests here.
+
+class LandingPageTest(TestCase):
+
+    def test_get(self):
+        # TODO some sort of test
+        response = self.client.get(reverse("landing-page"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "landing.html")
