763c143bf487b3671d6e1627335adc52503853ea
Raihan Ahmed Dip <raihan.dip@gmail.com>
2022-04-03 00:26:50 +0600

    Password Reset Function is implemented

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView, SignupView
 
@@ -29,6 +29,10 @@ urlpatterns = [
     path('agents/', include('agents.urls', namespace='agents')), 
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('passsword-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
